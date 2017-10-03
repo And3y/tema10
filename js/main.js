25cdb4359a2b7ef1eef3a69e31d3ffbb7f4f01cd
And3y <hoeghjr@gmail.com>
2017-10-03 15:14:55 +0200

    add art
    now we’re cooking with gas!

--- js/main.js
+++ js/main.js
@@ -7,15 +7,17 @@
 //        | ##   | ##  | ##|  ######/| ########| ########
 //        |__/   |__/  |__/ \______/ |________/|________/
 
-//                                              BY Team-AAs
+//                                              BY Team-AA
 
-var intro  = document.querySelectorAll('.intro');
-var first  = document.querySelectorAll('.first-group');
+var intro = document.querySelectorAll('.intro');
+var first = document.querySelectorAll('.first-group');
 var second = document.querySelectorAll('.second-group');
+var tekst = document.querySelector('.tekst');
+var info = document.querySelector('.infogfx');
 
 //  Hide all text
 for (var i = 0; i < intro.length; i++) {
-    intro[i].style.opacity = '0'; 
+    intro[i].style.opacity = '0';
 }
 
 // reveal first line
@@ -27,7 +29,7 @@ function fadeIn(arrayName) {
 }
 
 // fade text out
-function fadeItOut(arrayName){
+function fadeItOut(arrayName) {
     if (arrayName[1].classList.contains('fade-in')) {
         for (var i = 0; i < arrayName.length; i++) {
             arrayName[i].classList.remove('fade-in');
@@ -36,30 +38,45 @@ function fadeItOut(arrayName){
     }
 }
 
+// Hide text-div
+function hideText() {
+    tekst.style.display = 'none';
+}
+
+info.style.display = 'none';
+
+function showMap() {
+    info.style.display = 'block';
+    fadeIn(info);
+}
+
 // Get goin'
 
-setTimeout(function() {
+setTimeout(function () {
     fadeIn(first[1]);
 }, 2000)
 
 // fade out first group
-setTimeout(function() {
+setTimeout(function () {
     fadeItOut(first);
 }, 4000)
 
 // start of second group
-setTimeout(function() {
+setTimeout(function () {
     fadeIn(second[0]);
 }, 5000)
 
-setTimeout(function() {
+setTimeout(function () {
     fadeIn(second[1]);
 }, 6000)
 
-setTimeout(function() {
+setTimeout(function () {
     fadeIn(second[2]);
 }, 7000)
 // Fade out second group
-setTimeout(function() {
+setTimeout(function () {
     fadeItOut(second);
 }, 9000)
+
+setTimeout(hideText, 11000);
+setTimeout(showMap, 11000);
