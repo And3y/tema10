139234daa8e335c58050168ef3c13c938b4c90bd
And3y <hoeghjr@gmail.com>
2017-10-03 09:29:57 +0200

    Make all the text work

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@
 //        | $$   | $$  | $$|  $$$$$$/| $$$$$$$$| $$$$$$$$
 //        |__/   |__/  |__/ \______/ |________/|________/
 //
-//                                                 BY A&a
-var intro = document.querySelectorAll('.intro');
-var first = document.querySelectorAll('.first-group');
+//                                              BY Team-AAs
+
+var intro  = document.querySelectorAll('.intro');
+var first  = document.querySelectorAll('.first-group');
+var second = document.querySelectorAll('.second-group');
 
 //  Hide all text
 for (var i = 0; i < intro.length; i++) {
@@ -34,10 +36,34 @@ function fadeItOut(){
     }
 }
 
+function fadeItOutSecond(){
+    if (second[1].classList.contains('fade-in')) {
+        for (var i = 0; i < second.length; i++) {
+            second[i].classList.remove('fade-in');
+            second[i].classList.add('fade-out');
+        }
+    }
+}
+
 // Get goin'
 //setTimeout(fadeIn, 2000);
 setTimeout(function() {
     fadeIn(first[1]);
 }, 2000)
-
+// fade out first group
 setTimeout(fadeItOut, 4000);
+
+// start of second group
+setTimeout(function() {
+    fadeIn(second[0]);
+}, 5000)
+
+setTimeout(function() {
+    fadeIn(second[1]);
+}, 6000)
+
+setTimeout(function() {
+    fadeIn(second[2]);
+}, 7000)
+
+setTimeout(fadeItOutSecond, 8000);
